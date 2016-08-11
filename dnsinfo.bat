d16c28aab049f733e93107c1e0f5d68f2d92e15d
brett-rickman <brett.rickman@gmail.com>
2016-08-11 09:26:00 -0500

    Create dnsinfo.bat

---
+++ dnsinfo.bat
@@ -0,0 +1,9 @@
+@echo off
+rem
+rem Get information about DNS zones on this server
+rem
+del c:\windows\system32\dns\db.*
+dnscmd /enumzones > zonelist.txt
+for /F %%Z in ('dnscmd /enumzones /primary /secondary') DO dnscmd /ZoneExport %%Z db.%%Z
+copy c:\windows\system32\dns\db.*
+copy %systemroot%\system32\dns\DnsSettings.txt .
