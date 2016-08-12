c9dbca5a8459fdf73653a1554b5793da193fe7e0
brett-rickman <brett.rickman@gmail.com>
2016-08-12 11:40:03 -0500

    Corrected test for presence of Windows features

--- GatherData.ps1
+++ GatherData.ps1
@@ -1,17 +1,15 @@
 
-$features = Get-WindowsFeature
 $dns_reg_file = $env:ComputerName + "_dns.reg"
 $dns_csv_file = $env:ComputerName + "_dns_zones.csv"
-if ($features.DNS.Installed -eq 1) {
+if (Get-WindowsFeature DNS | where {$_.Installed -eq 1}) {
     echo "DNS Installed."
     reg export "HKLM\Software\Microsoft\Windows NT\CurrentVersion\DNS Server\Zones" $dns_reg_file
     Get-DnsServerZone | Export-Csv -Path $dns_csv_file -NoTypeInformation
     } else {
     echo "Skipping DNS collection because DNS role is not installed on $env:ComputerName."
     }
-if ($features.DHCP.Installed -eq 1) {
+if (Get-WindowsFeature DHCP | where {$_.Installed -eq 1}) {
     echo "DHCP Installed."
     } else {
     echo "Skipping DHCP collection because DHCP role is not installed on $env:ComputerName."
     }
-    
