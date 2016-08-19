7e80afcf72a912c166982c45fcc3c1d5f57b0d4b
brett-rickman <brett.rickman@gmail.com>
2016-08-19 11:02:32 -0500

    Added DHCP collection

--- dnsinfo.bat
+++ dnsinfo.bat
@@ -3,7 +3,8 @@ rem
 rem Get information about DNS zones on this server
 rem
 del c:\windows\system32\dns\db.*
-dnscmd /enumzones > zonelist.txt
+dnscmd /enumzones > %COMPUTERNAME%_zonelist.txt
 for /F %%Z in ('dnscmd /enumzones /primary /secondary') DO dnscmd /ZoneExport %%Z db.%%Z
 copy c:\windows\system32\dns\db.*
 copy %systemroot%\system32\dns\DnsSettings.txt .
+netsh dhcp server dump > %COMPUTERNAME%_netsh.txt
