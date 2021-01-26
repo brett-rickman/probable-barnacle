0626c1451ce73e98a8057dcd4b44d91dd8b34c91
brett-rickman <brett.rickman@gmail.com>
2021-01-26 12:35:34 -0600

    Add files via upload
    Uses CSV list of zones from AD and creates skeleton named.conf

--- proc_AD_zone_data.ps1
+++ proc_AD_zone_data.ps1
@@ -1,29 +1,49 @@
 param (
     [String]$FilePrefix = "db_202009011510_",
-    [String]$InDir = "./in",
-    [String]$ProcDir = "./proc",
-    [String]$OutDir = "./out",
-    [string]$LogDir = "./log"
-  
+    [String]$BaseDir = "work",
+    [string]$InDir = $BaseDir + "/" + "in",
+    [String]$ProcDir = $BaseDir + "/proc",
+    [String]$OutDir = $BaseDir + "/out",
+    [string]$LogDir = $BaseDir + "/log",
+    [string]$ZoneCsvFile =  "dns_zones.csv",
+    [string]$NamedConf = $BaseDir + "/" + "named.conf"
 )
 
-#Check for needed folders and create them if necessary
+# Template for named.conf entry
+$NamedTmpl = @"
+zone "__zone_name__" {
+    type __zone_type__;
+    file "__zone_file__";
+};
+
+"@
+#Check for needed folders
 $DirList = @("in","out","proc","log")
 foreach ($dir in $DirList) {
-    if ((test-path -path $dir) -eq $false) {
-        new-item -Path . -ItemType "directory" -Name $dir 
+    $TargetDir = join-path -path $BaseDir -childpath $dir 
+    if ((test-path -path $TargetDir) -eq $false) {
+        new-item -Path $BaseDir -ItemType "directory" -Name $dir 
     }
 }
-# Process each zone file located in $InDir
-# Assumes named-compilezone is somewhere in $env:path
-foreach ($zf in get-childitem $InDir) {
-    $zf_file = $InDir + "/" + $zf."Name"
-    $zone_name = $zf."Name" -replace $FilePrefix, ''
-    $proc_file = $ProcDir + "/" + $zf."Name" + ".proc"
-    $compiled_file = $OutDir + "/" + $zf."Name" + ".compilezone"
-    $ErrorFile = $LogDir + "/" + $zone_name + "_errors.log"
-    $WarningFile = $LogDir + "/" + $zone_name + "_warnings.log"
+# Process each zone 
+$ZoneCsv = $BaseDir + "/" + $ZoneCsvFile
+$ZoneList = import-csv $ZoneCsv
+foreach ($zone_entry in $ZoneList) {
+    $zone_name = $zone_entry."ZoneName"
+    if ( $zone_name -eq "0.in-addr.arpa" -or $zone_name -eq "255.in-addr.arpa" -or $zone_name -eq "127.in-addr.arpa" -or $zone_name -eq "TrustAnchors") {
+        continue
+    }
+    $zone_type = $zone_entry."ZoneType" -replace 'Primary', 'master'
+    $zf_file = $BaseDir + "/" + $FilePrefix + $zone_name 
+    $proc_file = $ProcDir + "/" + $FilePrefix + $zone_name + ".proc"
+    $compiled_file = $OutDir + "/" + $FilePrefix + $zone_name + ".compilezone"
+    $ErrorFile = $LogDir + "/" + $zone_name + "_zone_errors.log"
+    $WarningFile = $LogDir + "/" + $zone_name + "_zone_warnings.log"
+    $NamedEntry = $NamedTmpl -replace '__zone_name__', $zone_name
+    $NamedEntry = $NamedEntry -replace '__zone_type__', $zone_type
+    $NamedEntry = $NamedEntry -replace '__zone_file__', $compiled_file 
     get-content $zf_file | foreach-object { $_ -replace '\[AGE:\d+\]', ''} | out-file -FilePath $proc_file -encoding ascii 
     start-process -FilePath "named-compilezone" -NoNewWindow -RedirectStandardError $ErrorFile -RedirectStandardOutput $WarningFile `
     -ArgumentList "-k", "ignore", "-o", $compiled_file, $zone_name, $proc_file    
+    $NamedEntry | out-file -FilePath $NamedConf -encoding ascii -Append
 }
