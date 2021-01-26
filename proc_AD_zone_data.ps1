6deaa7ce14569a2839972d2c96aca7fca1e084e9
brett-rickman <brett.rickman@gmail.com>
2021-01-26 14:55:27 -0600

    Add files via upload
    Fixed bug in "file ..." entry for named.conf

--- proc_AD_zone_data.ps1
+++ proc_AD_zone_data.ps1
@@ -25,23 +25,27 @@ foreach ($dir in $DirList) {
         new-item -Path $BaseDir -ItemType "directory" -Name $dir 
     }
 }
-# Process each zone 
+# Process each zone in generated CSV file
 $ZoneCsv = $BaseDir + "/" + $ZoneCsvFile
 $ZoneList = import-csv $ZoneCsv
 foreach ($zone_entry in $ZoneList) {
     $zone_name = $zone_entry."ZoneName"
+    # Ignore built-in zones
     if ( $zone_name -eq "0.in-addr.arpa" -or $zone_name -eq "255.in-addr.arpa" -or $zone_name -eq "127.in-addr.arpa" -or $zone_name -eq "TrustAnchors") {
         continue
     }
+    # Translate zone type from MS to ISC
     $zone_type = $zone_entry."ZoneType" -replace 'Primary', 'master'
     $zf_file = $BaseDir + "/" + $FilePrefix + $zone_name 
     $proc_file = $ProcDir + "/" + $FilePrefix + $zone_name + ".proc"
     $compiled_file = $OutDir + "/" + $FilePrefix + $zone_name + ".compilezone"
     $ErrorFile = $LogDir + "/" + $zone_name + "_zone_errors.log"
     $WarningFile = $LogDir + "/" + $zone_name + "_zone_warnings.log"
+    # Build named.conf entry
     $NamedEntry = $NamedTmpl -replace '__zone_name__', $zone_name
     $NamedEntry = $NamedEntry -replace '__zone_type__', $zone_type
-    $NamedEntry = $NamedEntry -replace '__zone_file__', $compiled_file 
+    $NamedEntry = $NamedEntry -replace '__zone_file__', (Get-Item $compiled_file).Basename  
+    # Clean up zone file and validate it against BIND
     get-content $zf_file | foreach-object { $_ -replace '\[AGE:\d+\]', ''} | out-file -FilePath $proc_file -encoding ascii 
     start-process -FilePath "named-compilezone" -NoNewWindow -RedirectStandardError $ErrorFile -RedirectStandardOutput $WarningFile `
     -ArgumentList "-k", "ignore", "-o", $compiled_file, $zone_name, $proc_file    
